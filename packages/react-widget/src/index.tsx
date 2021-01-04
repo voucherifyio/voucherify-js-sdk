07d089c83913085d2ffb2f154df993468c701b89
Eddy Wilson <eddyw@users.noreply.github.com>
2021-01-05 03:43:38 +0800

    add base widget

---
+++ packages/react-widget/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export function VoucherifyValidate() {
+	return <div>Validate Widget</div>
+}
+export function VoucherifyRedeem() {
+	return <div>Redeem Widget</div>
+}
+export function VoucherifyPublish() {
+	return <div>Publish Widget</div>
+}
