fb7775397511cc2867f700bbcb29d28d68d00384
Eddy Wilson <eddyw@users.noreply.github.com>
2021-01-06 01:44:00 +0800

    update examples

--- examples/with-nodejs/client-track-event.js
+++ examples/with-nodejs/client-track-event.js
@@ -2,8 +2,8 @@
 const { VoucherifyClientSide } = require('@voucherify/sdk')
 
 const voucherify = VoucherifyClientSide({
-	applicationId: '011240bf-d5fc-4ef1-9e82-11eb68c43bf5',
-	clientToken: '9e2230c5-71fb-460a-91c6-fbee64707a20',
+	clientApplicationId: '011240bf-d5fc-4ef1-9e82-11eb68c43bf5',
+	clientSecretKey: '9e2230c5-71fb-460a-91c6-fbee64707a20',
 	trackingId: 'gustav@purpleson.com', // or voucherify.setIdentity('gustav@purpleson.com')
 	origin: 'example.com',
 })
