cb389de3e3be7e3e02e6b059ca67a6950f9ebf1e
Eddy Wilson <eddyw@users.noreply.github.com>
2021-01-06 01:43:48 +0800

    add jsdoc comments and rename options

--- packages/sdk/src/VoucherifyClientSide.ts
+++ packages/sdk/src/VoucherifyClientSide.ts
@@ -3,11 +3,55 @@ import { ClientSide } from './ClientSide'
 import { assert, isObject, isString, isOptionalString, environment } from './helpers'
 
 export interface VoucherifyClientSideOptions {
-	applicationId: string
-	clientToken: string
-	baseUrl?: string
+	/**
+	 * Optionally, you can add `apiUrl` to the client options if you want to use Voucherify running in a specific region.
+	 *
+	 * ```javascript
+	 * const client = VoucherifyClientSide({
+	 *		clientApplicationId: 'YOUR-CLIENT-APPLICATION-ID',
+	 *		clientSecretKey: 'YOUR-CLIENT-SECRET-KEY',
+	 *		apiUrl: 'https://<region>.api.voucherify.io'
+	 * })
+	 * ```
+	 */
+	apiUrl?: string
+	/**
+	 * [Log-in](http://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/login) to Voucherify web interface and obtain your `Client-side Keys` from [Configuration](https://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/app/configuration):
+	 *
+	 * ```javascript
+	 * const client = VoucherifyClientSide({
+	 *		clientApplicationId: 'YOUR-CLIENT-APPLICATION-ID',
+	 *		clientSecretKey: 'YOUR-CLIENT-SECRET-KEY'
+	 * })
+	 * ```
+	 */
+	clientApplicationId: string
+	/**
+	 * [Log-in](http://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/login) to Voucherify web interface and obtain your `Client-side Keys` from [Configuration](https://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/app/configuration):
+	 *
+	 * ```javascript
+	 * const client = VoucherifyClientSide({
+	 *		clientApplicationId: 'YOUR-CLIENT-APPLICATION-ID',
+	 *		clientSecretKey: 'YOUR-CLIENT-SECRET-KEY'
+	 * })
+	 * ```
+	 */
+	clientSecretKey: string
 	trackingId?: string
-	origin?: string // only on NodeJS
+	/**
+	 * **(Required in Node.js)** Set the origin from where the requests are made.
+	 *
+	 * ```javascript
+	 * const client = VoucherifyClientSide({
+	 *		clientApplicationId: 'YOUR-CLIENT-APPLICATION-ID',
+	 *		clientSecretKey: 'YOUR-CLIENT-SECRET-KEY',
+	 *		origin: 'your-domain.com'
+	 * })
+	 * ```
+	 *
+	 * @note in the browser, this option will be ignored. The `origin` header is a [forbidden header name](https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name) and it'll be automatically set by the browser for every request.
+	 */
+	origin?: string
 }
 export interface VoucherifyCustomerHeaders {
 	'X-Client-Application-Id': string
@@ -17,25 +61,25 @@ export interface VoucherifyCustomerHeaders {
 }
 export function VoucherifyClientSide(options: VoucherifyClientSideOptions) {
 	assert(isObject(options), 'VoucherifyCustomer: expected "options" argument to be an object')
-	assert(isString(options.applicationId), 'VoucherifyCustomer: "options.applicationId" is required')
-	assert(isString(options.clientToken), 'VoucherifyCustomer: "options.clientToken" is required')
-	assert(isOptionalString(options.baseUrl), 'VoucherifyCustomer: expected "options.baseUrl" to be a string')
+	assert(isString(options.clientApplicationId), 'VoucherifyCustomer: "options.clientApplicationId" is required')
+	assert(isString(options.clientSecretKey), 'VoucherifyCustomer: "options.clientSecretKey" is required')
+	assert(isOptionalString(options.apiUrl), 'VoucherifyCustomer: expected "options.baseUrl" to be a string')
 	assert(isOptionalString(options.trackingId), 'VoucherifyCustomer: expected "options.trackingId" to be a string')
 
 	const headers: VoucherifyCustomerHeaders = {
-		'X-Client-Application-Id': options.applicationId,
-		'X-Client-Token': options.clientToken,
-		'X-Voucherify-Channel': `${environment()}-Customer-SDK-v${__VERSION__}`,
+		'X-Client-Application-Id': options.clientApplicationId,
+		'X-Client-Token': options.clientSecretKey,
+		'X-Voucherify-Channel': `${environment()}-ClientSide-SDK-v${__VERSION__}`,
 	}
 
 	if (environment().startsWith('Node')) {
-		assert(isString(options.origin), 'VoucherifyCustomer: "options.origin" is required in NodeJS')
+		assert(isString(options.origin), 'VoucherifyCustomer: "options.origin" is required in Node.js')
 		headers['origin'] = options.origin
 	}
 
 	const client = new RequestController({
 		basePath: 'client/v1',
-		baseURL: options.baseUrl ?? 'https://api.voucherify.io',
+		baseURL: options.apiUrl ?? 'https://api.voucherify.io',
 		headers,
 	})
 
--- packages/sdk/src/VoucherifyServerSide.ts
+++ packages/sdk/src/VoucherifyServerSide.ts
@@ -20,11 +20,66 @@ import { Segments } from './Segments'
 import { assert, isString, isObject, isOptionalString, environment } from './helpers'
 
 export interface VoucherifyServerSideOptions {
-	baseUrl?: string
+	/**
+	 * Optionally, you can add `apiUrl` to the client options if you want to use Voucherify running in a specific region.
+	 *
+	 * ```javascript
+	 * const voucherify = VoucherifyServerSide({
+	 *		applicationId: 'YOUR-APPLICATION-ID',
+	 *		secretKey: 'YOUR-SECRET-KEY',
+	 *		apiUrl: 'https://<region>.api.voucherify.io'
+	 * })
+	 * ```
+	 */
+	apiUrl?: string
+	/**
+	 * [Log-in](http://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/login) to Voucherify web interface and obtain your `Application Keys` from [Configuration](https://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/app/configuration):
+	 *
+	 * ```javascript
+	 * const voucherify = VoucherifyServerSide({
+	 *		applicationId: 'YOUR-APPLICATION-ID',
+	 *		secretKey: 'YOUR-SECRET-KEY'
+	 * })
+	 * ```
+	 */
 	applicationId: string
-	clientSecretKey: string
+	/**
+	 * [Log-in](http://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/login) to Voucherify web interface and obtain your `Application Keys` from [Configuration](https://app.voucherify.io/?utm_source=github&utm_medium=sdk&utm_campaign=acq#/app/configuration):
+	 *
+	 * ```javascript
+	 * const voucherify = VoucherifyServerSide({
+	 *		applicationId: 'YOUR-APPLICATION-ID',
+	 *		secretKey: 'YOUR-SECRET-KEY'
+	 * })
+	 * ```
+	 */
+	secretKey: string
+	/**
+	 * Optionally, you can add `apiVersion` to the client options if you want to use a [specific API version](http://docs.voucherify.io/docs/api-version-upgrades?utm_source=github&utm_medium=sdk&utm_campaign=acq).
+	 *
+	 * ```javascript
+	 * const voucherify = VoucherifyServerSide({
+	 *		applicationId: 'YOUR-APPLICATION-ID',
+	 *		secretKey: 'YOUR-SECRET-KEY',
+	 *		apiVersion: 'v2017-04-20'
+	 * })
+	 * ```
+	 */
 	apiVersion?: string
 	channel?: string
+	/**
+	 * Set this option to disable displaying the warning about exposing your `secretKey` if you're using VoucherifyServerSide in a browser environment.
+	 * By setting this option to `true`, you acknowledge that you understand the risks of exposing your `secretKey` to a browser environment.
+	 *
+	 * ```javascript
+	 * const voucherify = VoucherifyServerSide({
+	 *		applicationId: 'YOUR-APPLICATION-ID',
+	 *		secretKey: 'YOUR-SECRET-KEY',
+	 *		dangerouslySetSecretKeyInBrowser: true
+	 * })
+	 * ```
+	 */
+	dangerouslySetSecretKeyInBrowser?: boolean
 }
 export interface VoucherifyServerSideHeaders {
 	'X-App-Id': string
@@ -34,15 +89,15 @@ export interface VoucherifyServerSideHeaders {
 	'Content-Type': 'application/json'
 }
 export function VoucherifyServerSide(options: VoucherifyServerSideOptions) {
-	assert(isObject(options), 'VoucherifyManager: the "options" argument must be an object')
-	assert(isString(options.applicationId), 'VoucherifyManager: "options.applicationId" is required')
-	assert(isString(options.clientSecretKey), 'VoucherifyManager: "options.clientSecretKey" is required')
-	assert(isOptionalString(options.apiVersion), 'VoucherifyManager: expected "options.apiVersion" to be a string')
-	assert(isOptionalString(options.channel), 'VoucherifyManager: expected "options.channel" to be a string')
+	assert(isObject(options), 'VoucherifyServerSide: the "options" argument must be an object')
+	assert(isString(options.applicationId), 'VoucherifyServerSide: "options.applicationId" is required')
+	assert(isString(options.secretKey), 'VoucherifyServerSide: "options.secretKey" is required')
+	assert(isOptionalString(options.apiVersion), 'VoucherifyServerSide: expected "options.apiVersion" to be a string')
+	assert(isOptionalString(options.channel), 'VoucherifyServerSide: expected "options.channel" to be a string')
 
 	const headers: VoucherifyServerSideHeaders = {
 		'X-App-Id': options.applicationId,
-		'X-App-Token': options.clientSecretKey,
+		'X-App-Token': options.secretKey,
 		'X-Voucherify-Channel': options.channel || `${environment()}-SDK-v${__VERSION__}`,
 		'Content-Type': 'application/json',
 	}
@@ -50,9 +105,21 @@ export function VoucherifyServerSide(options: VoucherifyServerSideOptions) {
 		headers['X-Voucherify-API-Version'] = options.apiVersion
 	}
 
+	/**
+	 * The option `dangerouslySetSecretKeyInBrowser` is explicitly long and not suggested in the thrown error because
+	 * we don't want the user to enable this option without going through the documentation and understanding the risks
+	 * of exposing a their secretKey
+	 */
+	if (!environment().startsWith('Node')) {
+		assert(
+			options.dangerouslySetSecretKeyInBrowser === true,
+			`VoucherifyServerSide: you're exposing your secretKey to a ${environment().toLowerCase()} environment. This is generally considered a bad practice. Did you mean to use 'VoucherifyClientSide'?`,
+		)
+	}
+
 	const client = new RequestController({
 		basePath: 'v1',
-		baseURL: options.baseUrl ?? 'https://api.voucherify.io',
+		baseURL: options.apiUrl ?? 'https://api.voucherify.io',
 		headers,
 	})
 	const balance = new Balance(client)
