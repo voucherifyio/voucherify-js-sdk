5617ec912f0bcdfa961baf52bc35df02440a385e
Piotr Gacek <pgacek0@gmail.com>
2021-01-15 14:16:33 +0100

    Added Segments types & missing Segment List POST method

--- packages/sdk/src/Segments.ts
+++ packages/sdk/src/Segments.ts
@@ -1,3 +1,5 @@
+import * as T from './types/Segments'
+
 import { encode } from './helpers'
 import type { RequestController } from './RequestController'
 
@@ -7,19 +9,28 @@ export class Segments {
 	/**
 	 * @see https://docs.voucherify.io/reference?utm_source=github&utm_medium=sdk&utm_campaign=acq#create-segment
 	 */
-	public create(segment: $FixMe) {
-		return this.client.post('/segments', segment)
+	public create(segment: T.SegmentsCreate) {
+		return this.client.post<T.SegmentsCreateResponse>('/segments', segment)
 	}
 	/**
 	 * @see https://docs.voucherify.io/reference?utm_source=github&utm_medium=sdk&utm_campaign=acq#get-segment
 	 */
 	public get(segmentId: string) {
-		return this.client.get(`/segments/${encode(segmentId)}`)
+		return this.client.get<T.SegmentsGetResponse>(`/segments/${encode(segmentId)}`)
 	}
 	/**
 	 * @see https://docs.voucherify.io/reference?utm_source=github&utm_medium=sdk&utm_campaign=acq#delete-segment
 	 */
 	public delete(segmentId: string) {
 		return this.client.delete(`/segments/${encode(segmentId)}`)
+	}
+
+	// I see that list method is missing
+
+	/**
+	 * @see https://docs.voucherify.io/reference?utm_source=github&utm_medium=sdk&utm_campaign=acq#list-segments
+	 */
+	public list(customerId: string) {
+		return this.client.delete(`/customers/${encode(customerId)}/segments`)
 	}
 }
