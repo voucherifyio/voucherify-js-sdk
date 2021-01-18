39190a2d0f047a7f62c864bafc92f34a9c65a946
Piotr Gacek <pgacek0@gmail.com>
2021-01-18 12:51:32 +0100

    Added Customers tests

--- packages/sdk/test/__mocks__/axios.ts
+++ packages/sdk/test/__mocks__/axios.ts
@@ -1,4 +1,8 @@
 const axios = jest.genMockFromModule('axios') as jest.Mocked<typeof import('axios')['default']>
 axios.create = jest.fn(() => axios)
-
 export default axios
+
+// eslint-disable-next-line import/no-anonymous-default-export
+// export default {
+// 	get: jest.fn(() => Promise.resolve({ data: null })),
+// }
--- packages/sdk/test/customers.spec.ts
+++ packages/sdk/test/customers.spec.ts
@@ -1,5 +1,6 @@
 import axiosOrg, { AxiosResponse } from 'axios'
 import { VoucherifyServerSide } from '../src'
+import { encode, omit } from '../src/helpers'
 
 const axios = axiosOrg as jest.Mocked<typeof import('axios')['default']>
 
@@ -18,5 +19,70 @@ describe('Customers API', () => {
 
 		expect(response).toBe(customer)
 		expect(axios.post).toHaveBeenCalledWith('/customers', customer, { params: undefined })
+	})
+
+	it('should get a customer', async () => {
+		const customerId = '12345'
+		const getCustomerResponse = { id: customerId, name: 'Customer Name' }
+
+		axios.get.mockResolvedValueOnce({ data: getCustomerResponse, status: 200 } as AxiosResponse)
+
+		const response = await voucherify.customers.get(customerId)
+
+		expect(response).toBe(getCustomerResponse)
+		expect(axios.get).toHaveBeenCalledWith(`/customers/${encode(customerId)}`, { params: undefined })
+	})
+
+	it('should list customers', async () => {
+		const customersList = { object: 'list', customers: [{ name: 'Test' }, { name: 'Test 2' }] }
+
+		axios.get.mockResolvedValueOnce({ data: customersList, status: 200 } as AxiosResponse)
+
+		const response = await voucherify.customers.list({})
+
+		expect(response).toBe(customersList)
+		expect(axios.get).toHaveBeenCalledWith('/customers', { params: {} })
+	})
+
+	it('should list more than 100 customers', async () => {
+		//@todo
+	})
+
+	it('should update customer', async () => {
+		const updateCustomer = { id: '098765', name: 'Changed Name' }
+
+		axios.put.mockResolvedValueOnce({ data: updateCustomer, status: 200 } as AxiosResponse)
+
+		const response = await voucherify.customers.update(updateCustomer)
+
+		expect(response).toBe(updateCustomer)
+		expect(axios.put).toHaveBeenCalledWith(`/customers/${encode(updateCustomer.id)}`, omit(updateCustomer, ['id']), {
+			params: undefined,
+		})
+	})
+
+	it('should delete a customer', async () => {
+		const deleteCustomerId = '098765'
+
+		axios.delete.mockResolvedValueOnce({ status: 200 } as AxiosResponse)
+
+		const response = await voucherify.customers.delete(deleteCustomerId)
+
+		expect(response).toBe(undefined)
+		expect(axios.delete).toHaveBeenCalledWith(`/customers/${encode(deleteCustomerId)}`, { params: undefined })
+	})
+
+	it('should update customer consents', async () => {
+		const updateCustomer = { id: '123456', name: 'Test Name' }
+		const updateConsent = { unsubscribed: true }
+
+		axios.put.mockResolvedValueOnce({ status: 200 } as AxiosResponse)
+
+		const response = await voucherify.customers.updateConsents(updateCustomer, updateConsent)
+
+		expect(response).toBe(undefined)
+		expect(axios.put).toHaveBeenCalledWith(`/customers/${encode(updateCustomer.id)}/consents`, updateConsent, {
+			params: undefined,
+		})
 	})
 })
