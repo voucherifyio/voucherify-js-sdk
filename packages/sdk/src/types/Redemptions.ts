c9e66bfac3bba8496bce133dcc8b4d2c8396b20e
Panga <pgacek0@gmail.com>
2021-02-11 10:54:38 +0100

    Updated Redemption types

--- packages/sdk/src/types/Redemptions.ts
+++ packages/sdk/src/types/Redemptions.ts
@@ -1,21 +1,6 @@
 import { OrdersItem, OrdersCreateResponse } from './Orders'
 import { SimpleCustomer } from './Customers'
-
-interface RedemptionsVoucherDiscountUnit {
-	type?: 'UNIT'
-	unit_off?: number
-	effect?: string
-}
-
-interface RedemptionsVoucherDiscountAmount {
-	type?: 'AMOUNT'
-	amount_off?: string
-}
-
-interface RedemptionsVoucherDiscountPercent {
-	type?: 'PERCENT'
-	percent_off?: number
-}
+import { VouchersResponse } from './Vouchers'
 
 export interface RedemptionsRedeemBody {
 	tracking_id?: string
@@ -43,75 +28,7 @@ export interface RedemptionsRedeemResponse {
 	order?: OrdersCreateResponse
 	metadata?: Record<string, any>
 	result?: string
-	voucher?: {
-		id: string
-		code: string
-		campaign?: string
-		category?: string
-		type?: 'DISCOUNT_VOUCHER' | 'GIFT_VOUCHER'
-		discount: RedemptionsVoucherDiscountAmount | RedemptionsVoucherDiscountPercent | RedemptionsVoucherDiscountUnit
-		gift?: {
-			amount: number
-		}
-		loyalty_card?: {
-			points: number
-		}
-		start_date?: string
-		expiration_date?: string
-		validity_timeframe?: {
-			interval: string
-			duration: string
-		}
-		validity_day_of_week?: number[]
-		publish?: {
-			object: 'list'
-			count: number
-			data_ref: 'entries'
-			entries: $FixMe[]
-			total: number
-			url: string
-		}
-		redemption?: {
-			object: 'list'
-			quantity?: number
-			redeemed_quantity: number
-			data_ref: 'redemption_entries'
-			redemption_entries: $FixMe[]
-			total: number
-			url: string
-		}
-		active: boolean
-		additional_info?: string
-		metadata?: Record<string, any>
-		assets?: {
-			qr?: {
-				id: string
-				url: string
-			}
-			barcode?: {
-				id: string
-				url: string
-			}
-		}
-		is_referral_code: boolean
-		referrer_id?: string
-		holder_id?: string
-		updated_at?: string
-		object: 'voucher'
-		validation_rules_assignments: {
-			object: 'list'
-			total: number
-			data_ref: 'data'
-			data?: {
-				id: string
-				rule_id?: string
-				related_object_id?: string
-				related_object_type?: string
-				created_at: string
-				object: 'validation_rules_assignment'
-			}[]
-		}
-	}
+	voucher?: VouchersResponse
 }
 
 export interface RedemptionsListParams {
@@ -126,7 +43,6 @@ export interface RedemptionsListParams {
 	}
 }
 
-// Here API returns string for 'total' as in other API examples it returns number
 export interface RedemptionsListResponse {
 	object: 'list'
 	total: number
@@ -203,33 +119,5 @@ export interface RedemptionsRollbackResponse {
 	redemption?: string
 	reason?: string
 	result?: string
-	voucher?: {
-		code?: string
-		campaign?: string
-		category?: string
-		type?: string
-		discount?: RedemptionsVoucherDiscountPercent | RedemptionsVoucherDiscountAmount | RedemptionsVoucherDiscountUnit
-		gift?: $FixMe
-		start_date?: string
-		expiration_date?: string
-		publish?: {
-			count?: number
-			entries?: $FixMe[]
-		}
-		redemption?: {
-			quantity?: number
-			redeemed_quantity?: number
-			redeemed_amount?: number
-			redemption_entries?: $FixMe[]
-		}
-		active?: boolean
-		additional_info?: string
-		metadata?: Record<string, any>
-		assets?: {
-			qr?: {
-				id?: string
-				url?: string
-			}
-		}
-	}
+	voucher?: VouchersResponse
 }
