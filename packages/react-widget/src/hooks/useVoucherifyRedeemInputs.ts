f7949a87be7c385c16117adbf3cb913c30b155db
pannga <pgacek0@gmail.com>
2021-03-19 17:20:55 +0100

    Redeem widget - version 2

---
+++ packages/react-widget/src/hooks/useVoucherifyRedeemInputs.ts
@@ -0,0 +1,53 @@
+import { VoucherifyRedeemInputs, VoucherifyRedeemInputsState } from '@voucherify/sdk'
+import { useCallback, useState } from 'react'
+
+function getEmptyInputs(): VoucherifyRedeemInputs {
+	return {
+		voucherifyCode: '',
+		voucherifyAmount: '',
+		voucherifyTracking: '',
+	}
+}
+
+function getEmptyInputState(): VoucherifyRedeemInputsState {
+	return {
+		voucherifyCode: false,
+		voucherifyAmount: false,
+		voucherifyRedeem: false,
+	}
+}
+
+function getEmptyValidInputState(): VoucherifyRedeemInputsState {
+	return {
+		voucherifyCode: false,
+		voucherifyAmount: false,
+		voucherifyRedeem: false,
+	}
+}
+
+export function useVoucherifyRedeemInputs() {
+	const [input, setInput] = useState(getEmptyInputs)
+	const [inputStates, setInputState] = useState(getEmptyInputState)
+	const [inputValidStates, setValidInputState] = useState(getEmptyValidInputState)
+
+	const onInputChange = useCallback(function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+		const name = event.target.name as keyof VoucherifyRedeemInputs
+		setInput(prev => ({ ...prev, [name]: event.target.value }))
+	}, [])
+
+	const resetInputs = useCallback(function reset() {
+		setInput(getEmptyInputs)
+		setInputState(getEmptyInputState)
+	}, [])
+
+	return {
+		input,
+		inputStates,
+		inputValidStates,
+		setValidInputState,
+		onInputChange,
+		resetInputs,
+		setInput,
+		setInputState,
+	}
+}
