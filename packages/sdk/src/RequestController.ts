cddd7b6da29680ec2d60e57e56ef47a85c18f5eb
pannga <pgacek0@gmail.com>
2021-03-24 15:52:16 +0100

    Removed withCredentials

--- packages/sdk/src/RequestController.ts
+++ packages/sdk/src/RequestController.ts
@@ -27,7 +27,6 @@ export class RequestController {
 			baseURL: `${this.baseURL}/${this.basePath}/`,
 			headers: this.headers,
 			responseType: 'json',
-			withCredentials: true,
 		})
 		this.request.interceptors.response.use(void 0, (error: AxiosError<unknown>) => {
 			/**
