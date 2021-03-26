375521679067007a17aeb8e6877e0ea42a17bac2
Piotr Gacek <pgacek0@gmail.com>
2021-03-26 15:53:18 +0100

    Examples (#53)
    * Changed file structure
    
    * Fixed minor issues
    
    * Changed file structure
    
    * Fixed minor issues

--- examples/sdk/with-nodejs-typescript/server-gift-voucher.ts
+++ examples/sdk/with-nodejs-typescript/server-gift-voucher.ts
@@ -1,5 +1,5 @@
 // require('source-map-support').install()
-const { VoucherifyServerSide } = require('@voucherify/sdk')
+import { VoucherifyServerSide } from '@voucherify/sdk'
 
 const voucherify = VoucherifyServerSide({
 	applicationId: 'c70a6f00-cf91-4756-9df5-47628850002b',
@@ -21,11 +21,11 @@ voucherify.vouchers
 	.then(function (result) {
 		console.log('Voucher %s created. Redeeming...', result.code)
 		voucherCode = result.code
-		return voucherify.redemptions.redeem(result.code, { order: { amount: 5000 } }, 'tester')
+		return voucherify.redemptions.redeem(result.code, { order: { amount: 5000 } })
 	})
 	.then(function (result) {
 		console.log('Voucher %s redeemed. Redemption id: %s, Rolling back...', result.voucher.code, result.id)
-		return voucherify.redemptions.rollback(result.id, 'just so', 'tester')
+		return voucherify.redemptions.rollback(result.id, { reason: 'Just so' })
 	})
 	.then(function (result) {
 		console.log('Redemption %s rolled back. Rollback id: %s', result.redemption, result.id)
