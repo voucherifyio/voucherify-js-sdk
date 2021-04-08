ec2349438cba43c809a239cce7ad9cd1353f0bd4
Piotr Gacek <pgacek0@gmail.com>
2021-04-08 10:05:40 +0200

    Updated `lerna`, `qs`, `typedoc` and `parcel` deps. Fixed minor bug with `examples/sdk/with-react/` configuration (#60)

--- examples/sdk/with-react/index.js
+++ examples/sdk/with-react/index.js
@@ -4,8 +4,8 @@ import * as ReactDOM from 'react-dom'
 import { VoucherifyClientSide } from '@voucherify/sdk'
 
 const voucherify = VoucherifyClientSide({
-	applicationId: '011240bf-d5fc-4ef1-9e82-11eb68c43bf5',
-	clientToken: '9e2230c5-71fb-460a-91c6-fbee64707a20',
+	clientApplicationId: '011240bf-d5fc-4ef1-9e82-11eb68c43bf5',
+	clientSecretKey: '9e2230c5-71fb-460a-91c6-fbee64707a20',
 	trackingId: 'gustav@purpleson.com', // or voucherify.setIdentity('gustav@purpleson.com')
 })
 
