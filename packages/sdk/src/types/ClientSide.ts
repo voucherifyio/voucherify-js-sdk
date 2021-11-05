080fd7117ca45b152a418608cd3366db16ee84d1
pannga <pgacek0@gmail.com>
2021-11-05 16:43:33 +0100

    Updated types

--- packages/sdk/src/types/ClientSide.ts
+++ packages/sdk/src/types/ClientSide.ts
@@ -135,8 +135,8 @@ export interface ClientSideTrackPayload {
 	event: string
 	metadata?: Record<string, any>
 	customer: CustomerRequest
-	loyalty?: ClientSideTrackLoyalty | null
-	referral?: ClientSideTrackReferral | null
+	loyalty?: ClientSideTrackLoyalty
+	referral?: ClientSideTrackReferral
 }
 
 export interface ClientSideTrackResponse {
