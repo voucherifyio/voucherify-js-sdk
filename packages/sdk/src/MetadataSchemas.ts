d5227cdc4f31b6d930b56f402181cca6f1d7d644
Dariusz Górak <darekg11@tlen.pl>
2022-08-09 08:35:10 +0200

    Add missing documentation links for MetadataSchemas API methods

--- packages/sdk/src/MetadataSchemas.ts
+++ packages/sdk/src/MetadataSchemas.ts
@@ -6,10 +6,16 @@ import type { RequestController } from './RequestController'
 export class MetadataSchemas {
 	constructor(private client: RequestController) {}
 
+	/**
+	 * @see https://docs.voucherify.io/reference/list-metadata-schemas
+	 */
 	public list() {
 		return this.client.get<T.MetadataSchemasListResponse>('/metadata-schemas')
 	}
 
+	/**
+	 * @see https://docs.voucherify.io/reference/get-metadata-schema
+	 */
 	public get(schemaName: string) {
 		return this.client.get<T.MetadataSchemasGetResponse>(`/metadata-schemas/${encode(schemaName)}`)
 	}
