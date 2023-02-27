488262a37b48b723f3f682ebbaa513cd88926831
Dariusz Górak <darekg11@tlen.pl>
2023-02-27 10:11:04 +0100

    patch - add explicit field definitions to VoucherifyError class so it is easier to use it with TypeScript typings

--- packages/sdk/src/VoucherifyError.ts
+++ packages/sdk/src/VoucherifyError.ts
@@ -2,6 +2,12 @@
  * @internal
  */
 export class VoucherifyError extends Error {
+	public code: string
+	public key: string
+	public details?: string
+	public request_id?: string
+	public resource_id?: string
+	public resource_type?: string
 	constructor(statusCode: number, body?: unknown) {
 		body = body ?? {}
 
@@ -9,7 +15,12 @@ export class VoucherifyError extends Error {
 
 		super(message)
 
-		Object.assign(this, body)
+		this.code = (<any>body).code
+		this.key = (<any>body).key
+		this.details = (<any>body).details
+		this.request_id = (<any>body).request_id
+		this.resource_id = (<any>body).resource_id
+		this.resource_type = (<any>body).resource_type
 	}
 }
 
