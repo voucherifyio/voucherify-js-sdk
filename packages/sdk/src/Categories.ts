ef05119eaec75f5e5899d11f1cb91c22397ea5b8
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-07-17 17:13:19 +0200

    categories

--- packages/sdk/src/Categories.ts
+++ packages/sdk/src/Categories.ts
@@ -1,6 +1,7 @@
 import * as T from './types/Categories'
 
 import type { RequestController } from './RequestController'
+import { encode } from './helpers'
 
 export class Categories {
 	constructor(private client: RequestController) {}
@@ -15,5 +16,19 @@ export class Categories {
 	 */
 	public create(promotionCampaign: T.CreateCategory) {
 		return this.client.post<T.ResponseCreateCategory>('/categories', promotionCampaign)
+	}
+	/**
+	 * @see https://docs.voucherify.io/reference/validate-promotions-1
+	 */
+	public get(id: string) {
+		return this.client.get<T.CategoryObject>(`/categories/${encode(id)}`)
+	}
+
+	public delete(id: string) {
+		return this.client.delete<{}>(`/categories/${encode(id)}`)
+	}
+
+	public update(id: string, body: T.UpdateCategoriesCategoryId) {
+		return this.client.put<T.CategoryObject>(`/categories/${encode(id)}`, body)
 	}
 }
