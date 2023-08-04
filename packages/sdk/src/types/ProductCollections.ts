6139aea668d5a9c9c6365e7009b939bf58e8946a
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-08-04 14:10:40 +0200

    Add '?' to ProductCollections and PromotionStacks

--- packages/sdk/src/types/ProductCollections.ts
+++ packages/sdk/src/types/ProductCollections.ts
@@ -12,60 +12,60 @@ export interface ListProductsCollectionParameters {
 }
 
 export interface ListProductCollections {
-	object: 'list'
-	data_ref: 'data'
-	data: ProductCollectionsObject[]
-	total: number
+	object?: 'list'
+	data_ref?: 'data'
+	data?: ProductCollectionsObject[]
+	total?: number
 }
 
 export interface ListProductsCollection {
-	object: 'list'
-	data_ref: 'data'
-	data: (ProductObject | SkuObjectExtended)[]
-	total: number
+	object?: 'list'
+	data_ref?: 'data'
+	data?: (ProductObject | SkuObjectExtended)[]
+	total?: number
 }
 
 interface SkuObjectExtended {
-	id: string
-	source_id: string
-	product_id: string
-	sku: string
-	price: number
-	attributes: Record<string, any>
-	image_url: string
-	metadata: Record<string, any>
-	created_at: string
-	updated_at: string
-	object: 'sku'
-	product: {
-		id: string
-		source_id: string
-		name: string
-		price: number
-		attributes: string[]
-		metadata: Record<string, any>
-		object: 'product'
+	id?: string
+	source_id?: string
+	product_id?: string
+	sku?: string
+	price?: number
+	attributes?: Record<string, any>
+	image_url?: string
+	metadata?: Record<string, any>
+	created_at?: string
+	updated_at?: string
+	object?: 'sku'
+	product?: {
+		id?: string
+		source_id?: string
+		name?: string
+		price?: number
+		attributes?: string[]
+		metadata?: Record<string, any>
+		object?: 'product'
 	}
 }
 
 export type ProductCollectionsObject = ProductCollectionsStaticObject | ProductCollectionsAutoUpdateObject
 
 interface ProductCollectionsStaticObject {
-	id: string
-	name: string
-	type: 'AUTO_UPDATE' | 'STATIC'
-	products: { id: string; object: 'product' }[]
-	created_at: string
-	updated_at: string
-	object: 'products_collection'
+	id?: string
+	name?: string
+	type?: 'AUTO_UPDATE' | 'STATIC'
+	products?: { id?: string; object?: 'product' }[]
+	created_at?: string
+	updated_at?: string
+	object?: 'products_collection'
 }
 
 interface ProductCollectionsAutoUpdateObject {
-	id: string
-	name: string
-	type: 'AUTO_UPDATE' | 'STATIC'
-	filter: Record<string, any>
-	created_at: string
-	updated_at: string
-	object: 'products_collection'
+	id?: string
+	name?: string
+	type?: 'AUTO_UPDATE' | 'STATIC'
+	filter?: Record<string, any>
+	created_at?: string
+	updated_at?: string
+	object?: 'products_collection'
 }
--- packages/sdk/src/types/PromotionStacks.ts
+++ packages/sdk/src/types/PromotionStacks.ts
@@ -17,33 +17,33 @@ export interface ListPromotionStacksParams {
 export type UpdatePromotionStack = Partial<CreatePromotionStack>
 
 export interface CreatePromotionStack {
-	name: string
+	name?: string
 	tiers?: {
-		ids: string[]
-		hierarchy_mode: string
+		ids?: string[]
+		hierarchy_mode?: string
 	}
 	category?: string
 	category_id?: string
 }
 
 export interface ListStacksInCampaignResponse {
-	object: 'list'
-	data_ref: 'data'
-	data: PromotionStackObject[]
-	total: number
+	object?: 'list'
+	data_ref?: 'data'
+	data?: PromotionStackObject[]
+	total?: number
 }
 
 export interface PromotionStackObject {
-	id: string
-	name: string
-	created_at: string
-	updated_at: string
-	campaign_id: string
-	object: 'promotion_stack'
-	tiers: {
-		ids: string[]
-		hierarchy_mode: 'MANUAL'
+	id?: string
+	name?: string
+	created_at?: string
+	updated_at?: string
+	campaign_id?: string
+	object?: 'promotion_stack'
+	tiers?: {
+		ids?: string[]
+		hierarchy_mode?: 'MANUAL'
 	}
-	category_id: string
-	categories: CategoryObject
+	category_id?: string
+	categories?: CategoryObject
 }
