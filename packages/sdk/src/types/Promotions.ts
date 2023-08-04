72646fbf44ac5bfad74c5f51ad2cf3c29badedd7
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-08-04 08:36:42 +0200

    remove unused types

--- packages/sdk/src/types/Promotions.ts
+++ packages/sdk/src/types/Promotions.ts
@@ -1,8 +1,8 @@
 import { DiscountAmount, DiscountPercent, DiscountUnit, DiscountFixed } from './DiscountVoucher'
 
-import { CreateOrder, OrdersItem, ValidateVoucherOrderId, ValidateVoucherOrderSourceId } from './Orders'
+import { CreateOrder } from './Orders'
 import { PromotionTier, RedeemPromotionTier, ValidationPromotionTierTrue } from './PromotionTiers'
-import { CreateCustomer, ValidateVoucherCustomerId, ValidateVoucherSourceId } from './Customers'
+import { CreateCustomer } from './Customers'
 import { ValidationRulesCreateAssignmentObject } from './ValidationRules'
 import { CategoryObject } from './Categories'
 
@@ -15,10 +15,9 @@ export type ValidatePromotionRequest = Partial<
 >
 
 export interface ResponseValidatePromotion {
-	//6_res_validate_promotion
 	valid: boolean
 	tracking_id: string
-	promotions: ValidationPromotionTierTrue & { category_id: string; categories: CategoryObject }
+	promotions?: ValidationPromotionTierTrue & { category_id?: string; categories?: CategoryObject }
 }
 
 export interface PromotionsCreateResponse {
@@ -77,45 +76,12 @@ export interface PromotionsCreate {
 }
 
 export interface PromotionsValidateParams {
-	customer: ValidateVoucherCustomerId | ValidateVoucherSourceId | CreateCustomer
-	order: ValidateVoucherOrderId | ValidateVoucherOrderSourceId | CreateOrder
+	customer: CreateCustomer
+	order: CreateOrder
 	metadata?: Record<string, any>
 }
 
 export interface PromotionsValidateQueryParams {
 	audienceRulesOnly?: boolean
 	filters?: Record<string, any>
-}
-
-export interface PromotionsValidateResponse {
-	valid: boolean
-	promotions?: {
-		id: string
-		object: 'promotion_tier'
-		banner?: string
-		name: string
-		start_date?: string
-		expiration_date?: string
-		discount?: DiscountUnit | DiscountAmount | DiscountPercent | DiscountFixed
-		discount_amount?: number
-		applied_discount_amount?: number
-		metadata?: Record<string, any>
-		order?: {
-			id?: string
-			source_id?: string
-			amount: number
-			initial_amount?: number
-			items_discount_amount?: number
-			items_applied_discount_amount?: number
-			items?: OrdersItem[]
-			metadata?: Record<string, any>
-			discount_amount?: number
-			total_discount_amount?: number
-			total_amount?: number
-			applied_discount_amount?: number
-			total_applied_discount_amount?: number
-		}
-		hierarchy?: number
-	}[]
-	tracking_id?: string
 }
