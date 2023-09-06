267f027c0e3e062ebf61cce6e3a684d8d0f296b5
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-09-06 10:00:36 +0200

    Update generateRandomString.ts

--- packages/sdk/test/utils/generateRandomString.ts
+++ packages/sdk/test/utils/generateRandomString.ts
@@ -1,5 +1,22 @@
-export const generateRandomString = (len = 40) => {
-	return Array(len + 1)
-		.join((Math.random().toString(36) + '00000000000000000').slice(2, 18))
-		.slice(0, len)
+export const generateRandomString = (length: number) => {
+	if (!length) {
+		throw new Error('length must be greater than zero')
+	}
+	if (length > 100) {
+		throw new Error('length must be smaller or equal to 100')
+	}
+	if (!Number.isInteger(length)) {
+		throw new Error('length must an integer')
+	}
+	function getRandomIntInclusive(min: number, max: number) {
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+	const seeds = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	const seedsLength = seeds.length
+	const tableOfStrings = []
+	while (tableOfStrings.length < length) {
+		const index = getRandomIntInclusive(0, seedsLength - 1)
+		tableOfStrings.push(seeds.slice(index, index + 1))
+	}
+	return tableOfStrings.join('')
 }
