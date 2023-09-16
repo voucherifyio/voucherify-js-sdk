0b7ca13f5628cc3e3ba8fb96fb0e0d453da0bc57
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-09-16 17:28:15 +0200

    Update loyalties.spec.ts

--- packages/sdk/test/loyalties.spec.ts
+++ packages/sdk/test/loyalties.spec.ts
@@ -43,6 +43,7 @@ describe('Loyalties API', () => {
 			object: 'balance',
 			related_object: { type: 'voucher', id: expect.stringMatching(/^v_.*/) },
 			operation_type: 'MANUAL',
+			// use as `type` for typescript check
 		} as LoyaltiesAddPointsResponse)
 	})
 
@@ -75,59 +76,7 @@ describe('Loyalties API', () => {
 				},
 			],
 			has_more: false,
+			// use as `type` for typescript check
 		} as LoyaltiesListLoyaltyCardTransactionsResponse)
 	})
-
-	// it('should get category', async () => {
-	// 	const category = await client.categories.get(categoryId)
-	// 	expect(category).toEqual({
-	// 		id: expect.stringMatching(/^cat_.*/),
-	// 		...newCategoryData,
-	// 		created_at: expect.stringMatching(isoRegex),
-	// 		object: 'category',
-	// 	})
-	// })
-	//
-	// it('should update category', async () => {
-	// 	const updatedCategoryData = { name: generateRandomString(50), hierarchy: 33 }
-	// 	const category = await client.categories.update(categoryId, updatedCategoryData)
-	// 	expect(category).toEqual({
-	// 		id: expect.stringMatching(/^cat_.*/),
-	// 		...updatedCategoryData,
-	// 		created_at: expect.stringMatching(isoRegex),
-	// 		object: 'category',
-	// 		updated_at: expect.stringMatching(isoRegex),
-	// 	})
-	// })
-	//
-	// it('should list categories', async () => {
-	// 	const categoriesList = await client.categories.list()
-	// 	expect(categoriesList).toMatchObject(
-	// 		expect.objectContaining({
-	// 			object: 'list',
-	// 			data_ref: 'data',
-	// 			has_more: false,
-	// 		}),
-	// 	)
-	// 	expect(typeof categoriesList.total).toBe('number')
-	// 	expect(Array.isArray(categoriesList.data)).toBeTruthy()
-	// 	categoriesList.data.forEach(category => {
-	// 		const expectObjectContaining = expect.objectContaining({
-	// 			id: expect.stringMatching(/[\s\S]*/),
-	// 			created_at: expect.stringMatching(isoRegex),
-	// 			name: expect.stringMatching(/[\s\S]*/),
-	// 			object: 'category',
-	// 		})
-	// 		expect(category).toEqual(expectObjectContaining)
-	// 		expect(typeof category.hierarchy).toBe('number')
-	// 		if (category.updated_at) {
-	// 			expect(category.updated_at).toEqual(expect.stringMatching(isoRegex))
-	// 		}
-	// 	})
-	// })
-	//
-	// it('should delete category', async () => {
-	// 	await client.categories.delete(categoryId)
-	// 	await expect(client.categories.get(categoryId)).rejects.toThrow('Resource not found')
-	// })
 })
